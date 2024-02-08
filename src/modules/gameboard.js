3ce672e92bdb92f5de1634b1dfa9017deeb2789a
Anton <anton20020426@gmail.com>
2024-02-08 08:51:07 +0100

    Save a functional app version before "reserveAdjacentCells"

---
+++ src/modules/gameboard.js
@@ -0,0 +1,180 @@
+import { Ship } from './ship';
+export { Gameboard };
+
+class Gameboard {
+  constructor() {
+    this.board = this.#createGameboard();
+    this.allShips = 0;
+    this.sunkShips = 0;
+  }
+
+  #createGameboard() {
+    let board = [];
+    for (let i = 0; i < 10; i++) {
+      board[i] = [];
+      for (let j = 0; j < 10; j++) {
+        board[i].push([
+          {
+            containsShip: false,
+            adjacentToNearestShip: false,
+            missed: false,
+            adjacentToSunkShip: false,
+          },
+        ]);
+      }
+    }
+    return board;
+  }
+
+  #fitInBoardLimits(coordinateX, coordinateY) {
+    if (Array.isArray(coordinateX) && Array.isArray(coordinateY)) {
+      const xIsOutsideBoard = coordinateX.filter((x) => x > 9);
+      const yIsOutsideBoard = coordinateY.filter((y) => y > 9);
+      if (xIsOutsideBoard.length || yIsOutsideBoard.length) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+
+    if (
+      coordinateX > 9 ||
+      coordinateY > 9 ||
+      coordinateX < 0 ||
+      coordinateY < 0
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  #reserveAdjacentCoordinates(coordinateX, coordinateY) {
+    const adjacentCoordinatesOfX = [-1, 0, 1, 1, 1, 0, -1, -1];
+    const adjacentCoordinatesOfY = [-1, -1, -1, 0, 1, 1, 1, 0];
+
+    for (let i = 0; i < 8; i++) {
+      const x = coordinateX + adjacentCoordinatesOfX[i];
+      const y = coordinateY + adjacentCoordinatesOfY[i];
+
+      if (x > 9 || y > 9 || x < 0 || y < 0) {
+        continue;
+      } else {
+        this.board.at(x).at(y).at(0).adjacentToNearestShip = true;
+      }
+    }
+  }
+
+  #confirmCoordinatesAreNotAvailable(coordinateX, coordinateY) {
+    let areNotAvailableCoordinates;
+    if (Array.isArray(coordinateX) && Array.isArray(coordinateY)) {
+      for (let i = 0; i < coordinateX.length; i++) {
+        areNotAvailableCoordinates = this.board
+          .at(coordinateX[i])
+          .at(coordinateY[i])
+          .at(0).containsShip;
+
+        if (areNotAvailableCoordinates) {
+          return true;
+        }
+      }
+      return false;
+    } else {
+      areNotAvailableCoordinates = this.board
+        .at(coordinateX)
+        .at(coordinateY)
+        .at(0).containsShip;
+
+      if (areNotAvailableCoordinates) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  #enterAnotherShipCoordinates(coordinateX, coordinateY) {
+    let areReservedCoordinates;
+    if (Array.isArray(coordinateX) && Array.isArray(coordinateY)) {
+      for (let i = 0; i < coordinateX.length; i++) {
+        areReservedCoordinates = this.board
+          .at(coordinateX[i])
+          .at(coordinateY[i])
+          .at(0).adjacentToNearestShip;
+
+        if (areReservedCoordinates) {
+          return true;
+        }
+      }
+      return false;
+    } else {
+      areReservedCoordinates = this.board
+        .at(coordinateX)
+        .at(coordinateY)
+        .at(0).adjacentToNearestShip;
+
+      if (areReservedCoordinates) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  placeShip(x, y, shipSize, direction = 'hor') {
+    if (!this.#fitInBoardLimits(x, y)) {
+      return 'Cannot place the ship outside the board';
+    }
+
+    if (this.#confirmCoordinatesAreNotAvailable(x, y)) {
+      return 'Cannot place the ship in cells taken by another ship';
+    }
+
+    const ship = new Ship(shipSize);
+    if (ship.length > 1) {
+      const shiftedX = [];
+      const shiftedY = [];
+
+      if (direction === 'hor') {
+        for (let i = 0; i < ship.length; i++) {
+          shiftedX.push(x);
+          shiftedY.push(y);
+          x += 1;
+        }
+      } else if (direction === 'ver') {
+        for (let i = 0; i < ship.length; i++) {
+          shiftedX.push(x);
+          shiftedY.push(y);
+          y += 1;
+        }
+      }
+
+      if (!this.#fitInBoardLimits(shiftedX, shiftedY)) {
+        return 'Cannot place the ship outside the board';
+      }
+
+      if (this.#confirmCoordinatesAreNotAvailable(shiftedX, shiftedY)) {
+        return 'Cannot place the ship in cells taken by another ship';
+      }
+
+      // Make sure there is a gap between two ships
+      // for (let i = 0; i < ship.length; i++) {
+      //   this.#reserveAdjacentCoordinates(shiftedX[i], shiftedY[i]);
+      // }
+
+      // if (this.#enterAnotherShipCoordinates(shiftedX, shiftedY)) {
+      //   return 'Cannot place the ship right beside another ship';
+      // }
+
+      for (let i = 0; i < ship.length; i++) {
+        this.board.at(shiftedX[i]).at(shiftedY[i]).at(0).containsShip = ship;
+      }
+    } else if (ship.length === 1) {
+      // if (this.#enterAnotherShipCoordinates(x, y)) {
+      //   return 'Cannot place the ship right beside another ship';
+      // } else {
+      // }
+      this.board.at(x).at(y).at(0).containsShip = ship;
+    }
+  }
+}
---
+++ src/tests/gameboard.test.js
@@ -0,0 +1,87 @@
+// import { Ship } from './ship';
+import { Gameboard } from '../modules/gameboard';
+
+describe('Test public interface of a gameboard', () => {
+  test('placeShip() places a ship with the length of 1 inside the correct coordinates', () => {
+    const gameboard = new Gameboard();
+
+    expect(gameboard.board.at(0).at(0).at(0).containsShip).toBeFalsy();
+    gameboard.placeShip(0, 0, 1);
+
+    expect(gameboard.board.at(0).at(0).at(0).containsShip).toBeTruthy();
+  });
+
+  test('placeShip() places a ship with the length of > 1 inside the correct coodinates', () => {
+    const gameboard = new Gameboard();
+
+    expect(gameboard.board.at(3).at(4).at(0).containsShip).toBeFalsy();
+    gameboard.placeShip(3, 4, 3);
+
+    expect(gameboard.board.at(3).at(4).at(0).containsShip).toBeTruthy();
+    expect(gameboard.board.at(4).at(4).at(0).containsShip).toBeTruthy();
+    expect(gameboard.board.at(5).at(4).at(0).containsShip).toBeTruthy();
+  });
+
+  test('placeShip() places a ship in both directions: horizontally and vertically', () => {
+    const gameboard = new Gameboard();
+
+    expect(gameboard.board.at(0).at(7).at(0).containsShip).toBeFalsy();
+    gameboard.placeShip(0, 7, 2, 'ver');
+
+    expect(gameboard.board.at(1).at(7).at(0).containsShip).toBeFalsy();
+    expect(gameboard.board.at(0).at(7).at(0).containsShip).toBeTruthy();
+    expect(gameboard.board.at(0).at(8).at(0).containsShip).toBeTruthy();
+  });
+
+  test('placeShip() cannot place ships outside the board', () => {
+    const gameboard = new Gameboard();
+
+    expect(gameboard.placeShip(-1, -1, 1)).toBe(
+      'Cannot place the ship outside the board'
+    );
+    expect(gameboard.placeShip(9, 0, 3, 'hor')).toBe(
+      'Cannot place the ship outside the board'
+    );
+    expect(gameboard.placeShip(0, 9, 2, 'ver')).toBe(
+      'Cannot place the ship outside the board'
+    );
+  });
+
+  test('placeShip() cannot place a ship in squares occupied by another ship', () => {
+    const gameboard = new Gameboard();
+
+    gameboard.placeShip(0, 0, 1);
+    expect(gameboard.placeShip(0, 0, 3)).toBe(
+      'Cannot place the ship in cells taken by another ship'
+    );
+
+    gameboard.placeShip(3, 4, 3);
+    expect(gameboard.placeShip(5, 4, 1)).toBe(
+      'Cannot place the ship in cells taken by another ship'
+    );
+
+    gameboard.placeShip(0, 7, 2);
+    expect(gameboard.placeShip(0, 5, 4, 'ver')).toBe(
+      'Cannot place the ship in cells taken by another ship'
+    );
+  });
+
+  test('placeShip() cannot place ships next to each other. Mix gap must be 1 cell', () => {
+    const gameboard = new Gameboard();
+
+    gameboard.placeShip(0, 0, 1);
+    expect(gameboard.placeShip(1, 1, 4)).toBe(
+      'Cannot place the ship right beside another ship'
+    );
+
+    gameboard.placeShip(3, 4, 3, 'hor');
+    expect(gameboard.placeShip(3, 5, 1)).toBe(
+      'Cannot place the ship right beside another ship'
+    );
+
+    gameboard.placeShip(8, 8, 2, 'ver');
+    expect(gameboard.placeShip(8, 5, 4, 'ver')).toBe(
+      'Cannot place the ship right beside another ship'
+    );
+  });
+});
