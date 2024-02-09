f3cde5faea571f60e07e8ca50cc1b1de6e87258d
Anton <anton20020426@gmail.com>
2024-02-09 10:12:32 +0100

    Add some useful comments where my intentions might not be clear

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -13,6 +13,7 @@ class Gameboard {
     for (let i = 0; i < 10; i++) {
       board[i] = [];
       for (let j = 0; j < 10; j++) {
+        // Create an object in each square of the board to control its future flow
         board[i].push([
           {
             containsShip: false,
@@ -32,6 +33,7 @@ class Gameboard {
     const coordinateX = arrayWithArguments[0];
     const coordinateY = arrayWithArguments[1];
 
+    // Check limits of ships of length 1 as well as 2,3,4 (the latter will be created with coordinate combinations in two arrays)
     if (Array.isArray(coordinateX) && Array.isArray(coordinateY)) {
       const xIsOutsideBoard = coordinateX.filter((x) => x > 9);
       const yIsOutsideBoard = coordinateY.filter((y) => y > 9);
@@ -59,6 +61,7 @@ class Gameboard {
     const coordinateX = arrayWithArguments[0];
     const coordinateY = arrayWithArguments[1];
 
+    // Use two arrays filled with all possible adjacent squares of the coordinates
     const adjacentCoordinatesOfX = [-1, 0, 1, 1, 1, 0, -1, -1];
     const adjacentCoordinatesOfY = [-1, -1, -1, 0, 1, 1, 1, 0];
 
@@ -66,10 +69,10 @@ class Gameboard {
       const x = coordinateX + adjacentCoordinatesOfX[i];
       const y = coordinateY + adjacentCoordinatesOfY[i];
 
-      // Also if it tries to "reserve" coordinates that belong to the same ship, don't
       if (x > 9 || y > 9 || x < 0 || y < 0) {
         continue;
       } else if (this.board.at(x).at(y).at(0).containsShip) {
+        // If the square is a part of the ship, its continuation, it's not adjacent. It contains a ship
         continue;
       } else {
         this.board.at(x).at(y).at(0).adjacentToNearestShip = true;
@@ -93,8 +96,8 @@ class Gameboard {
     return resultOfHelper;
   }
 
+  // Use the rest operator to place all arguments into one array and then extract specific parts of it
   #accessPropertyOnShipObject(...coordinatesAndProperty) {
-    // Use the rest operator to place all arguments into one array and then extract specific parts of it
     const arrayWithArguments = coordinatesAndProperty;
     const coordinateX = arrayWithArguments[0][0];
     const coordinateY = arrayWithArguments[0][1];
@@ -106,7 +109,7 @@ class Gameboard {
         willCancelShipCreation = this.board
           .at(coordinateX[i])
           .at(coordinateY[i])
-          .at(0)[property];
+          .at(0)[property]; // Access a property on the ship with which the method was called
 
         if (willCancelShipCreation) {
           return true;
@@ -136,15 +139,18 @@ class Gameboard {
     }
 
     if (ship.length > 1) {
+      // Create two arrays to place ships over multiple squares, doing it in both directions.
       const shiftedX = [];
       const shiftedY = [];
 
+      // If placed horizontally, the ship's first square will generate the rest of squares to the right, without changing the row
       if (direction === 'hor') {
         for (let i = 0; i < ship.length; i++) {
           shiftedX.push(x);
           shiftedY.push(y);
           x += 1;
         }
+        // If placed vertically, the ship's first square will generate the rest of squares below, without changing the column
       } else if (direction === 'ver') {
         for (let i = 0; i < ship.length; i++) {
           shiftedX.push(x);
@@ -153,7 +159,7 @@ class Gameboard {
         }
       }
 
-      // Check if any of these restrictions is broken before creating the ship
+      // Check if any of these restrictions is broken before placing a ship. If yes, the ship is not placed on the board
       if (!this.#fitInBoardLimits(shiftedX, shiftedY)) {
         return 'Cannot place the ship outside the board';
       } else if (this.#confirmCoordinatesAreNotAvailable(shiftedX, shiftedY)) {
@@ -162,12 +168,13 @@ class Gameboard {
         return 'Cannot place the ship right beside another ship';
       }
 
-      // Make sure there is a gap between two ships
+      // Make sure there is one square of a gap between two ships (in all directions)
       for (let i = 0; i < ship.length; i++) {
         this.board.at(shiftedX[i]).at(shiftedY[i]).at(0).containsShip = ship;
       }
 
-      // Get rid of this loop here and move in inside the method???
+      // Refactore this part???
+      // Get rid of this loop here and move in inside reserveAdjacentCoordinates()
       for (let i = 0; i < ship.length; i++) {
         this.#reserveAdjacentCoordinates(shiftedX[i], shiftedY[i]);
       }
@@ -230,9 +237,11 @@ class Gameboard {
       return 'Coordinates have been targetted already';
     }
 
+    // Target the provided square successfully
     this.board.at(x).at(y).at(0).targetted = true;
 
     if (this.#targetShip(arrayWithCoordinates)) {
+      // Don't forget!!!
       // hit() is going to be called here
       return 'Ship was targetted';
     } else {
